437b8000b999a27d367a6654b776257050b88bd6
Lucien Rae <lucienraegentil@gmail.com>
2017-07-18 18:52:01 +1000

    Made mesh_step_size and k parameters

--- side/brain-animation-test.py
+++ side/brain-animation-test.py
@@ -13,8 +13,8 @@ style.use('ggplot')
 
 class Brain:
 
-	def __init__(self, k=3, *args, **kwargs):
-		self.k = k
+	def __init__(self, *args, **kwargs):
+		self.k = 3
 		self.clf = KNeighborsClassifier(self.k)
 		self.X = np.array([])
 		self.y = np.array([])
@@ -50,13 +50,11 @@ class Brain:
 
 		print("Fitted {} samples [{}s]".format(len(self.y),round(time.time() - start_time,5)))
 
-	def visualize(self, show_text=True, mesh_step_size=0.1):
-		# step size in the mesh
-
+	def visualize(self, mesh_step_size=0.1):
 		start_time = time.time()
 
-		x_min, x_max = self.X[:, 0].min() - 0.2, self.X[:, 0].max() + 0.2
-		y_min, y_max = self.X[:, 1].min() - 0.2, self.X[:, 1].max() + 0.2
+		x_min, x_max = self.X[:, 0].min() - 0.4, self.X[:, 0].max() + 0.4
+		y_min, y_max = self.X[:, 1].min() - 0.4 , self.X[:, 1].max() + 0.4
 		xx, yy = np.meshgrid(np.arange(x_min, x_max, mesh_step_size), np.arange(y_min, y_max, mesh_step_size))
 
 		# plot setup
@@ -73,7 +71,7 @@ class Brain:
 		self.Z = self.clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:, 1]
 		self.Z = self.Z.reshape(xx.shape)
 
-		print("Calculated predictions ({}) [{}s]".format(len(xx)*len(yy), round(time.time() - start_time,5)))
+		print("Calculated predictions ({}) [{}s]".format(len(self.Z), round(time.time() - start_time,5)))
 		start_time = time.time()
 
 		# plot Z as contour
@@ -83,12 +81,11 @@ class Brain:
 		ax.scatter(self.X[:, 0], self.X[:, 1], c=self.y, cmap=cm_bold, marker="x")
 		ax.set_xlim(xx.min(), xx.max())
 		ax.set_ylim(yy.min(), yy.max())
-		ax.set_xticks(())
+		ax.set_xticks(()) 
 		ax.set_yticks(())
-
-		if show_text:
-			ax.text(xx.min() + 0.1, yy.min() + 0.1, "n_samples={}".format(len(self.y)), size=12, horizontalalignment='left')
-			ax.text(xx.max() - 0.1, yy.min() + 0.1, "k={}".format(self.k), size=12, horizontalalignment='right')
+		ax.text(xx.min() + 0.1, yy.max() - 0.2, "mesh_step_size={}".format(mesh_step_size), size=12, horizontalalignment='left')
+		ax.text(xx.min() + 0.1, yy.min() + 0.1, "n_samples={}".format(len(self.y)), size=12, horizontalalignment='left')
+		ax.text(xx.max() - 0.1, yy.min() + 0.1, "k={}".format(self.k), size=12, horizontalalignment='right')
 
 		print("Prepared visualization [{}s]".format(round(time.time() - start_time,5)))
 
