1ba4c91e7f7c452d2a5798d124465eb54c3c635d
Lucien Rae <lucienraegentil@gmail.com>
2017-07-24 19:52:52 +1000

    Brain visualization from eye visualization

--- main.py
+++ main.py
@@ -24,16 +24,24 @@ class Window:
 
 		# init display
 		self.display_width = 32*40
-		self.display_height = 32*30
+		self.display_height = 32*25
 		self.side_display_width = 32*20
 		self.side_display_height = 32*0
 		self.display = pygame.display.set_mode((self.display_width+self.side_display_width, self.display_height+self.side_display_height))
 
 		# init misc
 		self.clock = pygame.time.Clock()
-		self.world_speed = 144 # default = 144
 		self.grid_size = 32
 		self.total_steps = 0
+
+		full_capacity = True
+
+		if full_capacity:
+			self.world_speed = 300
+			self.step_division = 1
+		else:
+			self.world_speed = 30
+			self.step_division = 10
 
 		# init focus
 		self.focus = None # default = None
@@ -50,6 +58,7 @@ class Window:
 		# init colors
 		self.WHITE = (255, 255, 255)
 		self.GRAY = (225, 225, 225)
+		self.DK_GRAY = (122, 122, 122)
 		self.BLACK = (0, 0, 0)
 		self.RED = (255, 0, 0)
 		self.GREEN = (0, 255, 0)
@@ -60,6 +69,7 @@ class Window:
 		self.LT_GREEN = (122, 255, 122)
 
 		# init font
+		self.FNT_TINY = pygame.font.SysFont("arial", 9)
 		self.FNT_SMALL = pygame.font.SysFont("arial", 11)
 		self.FNT_MEDIUM = pygame.font.SysFont("arial", 14)
 		self.FNT_LARGE = pygame.font.SysFont("arial", 16)
@@ -71,9 +81,10 @@ class Window:
 		self.sentiments = {-2: "very negative", -1: "negative", 0: "neutral", 1: "positive", 2: "very positive"}
 		self.sentiment_colors = {-2: self.RED, -1: self.LT_RED, 0: self.GRAY, 1: self.LT_GREEN, 2: self.GREEN}
 		self.sentiment_colors_alt = {-2: self.RED, -1: self.LT_RED, 0: self.BLACK, 1: self.LT_GREEN, 2: self.GREEN}
+		self.biomes = {-1: "limbo", 0: "bme0", 1: "bme1", 2: "bme2", 3: "bme3"}
 
 		# init agents
-		starting_agents = 32
+		starting_agents = 30
 		self.agents = []
 		for i in range(starting_agents):
 			self.create_agent(i)
@@ -90,64 +101,67 @@ class Window:
 			agent_pos_list.append((agent[4], agent[5]))
 
 		for agent in self.agents:
-			eye = agent[6]
-			brain = agent[7]
-			muscle = agent[8]
+			if (self.total_steps+agent[0]) % self.step_division == 0:
+				eye = agent[6]
+				brain = agent[7]
+				muscle = agent[8]
 
-			# process any experience had
-			agent_contacts = []
-			for a in enumerate(agent_pos_list):
-				# check for contact with another agent
-				if a[1] == (agent[4], agent[5]) and a[0] != agent[0]:
-					other_agent = a[0]
+				# process any experience had
+				agent_contacts = []
+				for a in enumerate(agent_pos_list):
+					# check for contact with another agent
+					if a[1] == (agent[4], agent[5]) and a[0] != agent[0]:
+						other_agent = a[0]
 
-					other_color = self.agents[other_agent][2]
-					other_shape = self.agents[other_agent][3]
-					other_x = self.agents[other_agent][4]
-					other_y = self.agents[other_agent][5]
+						other_color = self.agents[other_agent][2]
+						other_shape = self.agents[other_agent][3]
+						other_x = self.agents[other_agent][4]
+						other_y = self.agents[other_agent][5]
+						other_biome = -1
 
-					# get features
-					X_1 = other_color
-					X_2 = other_shape
+						# get features
+						X_1 = other_color
+						X_2 = other_shape
+						X_3 = other_biome
 
-					# decide sentiment (label)
-					sent = self.decide_sentiment(agent, other_color, other_shape)
+						# decide sentiment (label)
+						sent = self.decide_sentiment(agent, X_1, X_2, X_3)
 
-					# experience
-					self.experience(agent, [X_1, X_2], sent)
+						# experience
+						self.experience(agent, [X_1, X_2, X_3], sent)
 
-					if brain.total_experiences() % self.brain_map_frequency == 0:
-						try:
-							new_sent = agent[7].predict([[X_1, X_2]])
-							agent[9][X_1][X_2] = new_sent
-						except:
-							pass
+						if brain.total_experiences() % self.brain_map_frequency == 0:
+							try:
+								new_sent = agent[7].predict([[X_1, X_2, X_3]])
+								agent[9][X_1][X_2] = new_sent # push to brain map
+							except:
+								pass
 
-					# print experience
-					if agent[0] == self.focus:
-						print("[AGENT#{}] Experience w/ #{}! sentiment={}".format(agent[0], other_agent, self.sentiments[sent]))
-						self.focus_msg = ("#{}, {}, {}, {}".format(other_agent, self.color_names[other_color], self.shapes[other_shape], self.sentiments[sent]), sent)
+						# print experience
+						if agent[0] == self.focus:
+							print("X = [{} {} {}], y = {}".format(X_1, X_2, X_3 , self.sentiments[sent]))
+							self.focus_msg = ("#{}, {}, {}, {}: {}".format(other_agent, self.color_names[other_color], self.shapes[other_shape], self.biomes[other_biome], self.sentiments[sent]), sent)
 
-					# learn from experiences
-					if brain.total_experiences() > 3:
-						brain.learn()
+						# learn from experiences
+						if brain.total_experiences() > 3:
+							brain.learn()
 
-						if agent[0] == self.focus and self.focus_graphs and brain.total_experiences() % self.focus_visualize_frequency == 0:
-							# self.check_agent(agent[0])
-							brain.visualize("AGENT#{}: {}".format(agent[0], agent[1]), time_limit=self.focus_visualize_time)
-	
-			# percieve area
-			potential_cells = eye.look(agent[4], agent[5])
+							if agent[0] == self.focus and self.focus_graphs and brain.total_experiences() % self.focus_visualize_frequency == 0:
+								# self.check_agent(agent[0])
+								brain.visualize("AGENT#{}: {}".format(agent[0], agent[1]), time_limit=self.focus_visualize_time)
+		
+				# percieve area
+				potential_cells = eye.look(agent[4], agent[5])
 
-			# calculate best cell to move to
-			move_cell = random.choice(potential_cells)
+				# calculate best cell to move to
+				move_cell = random.choice(potential_cells)
 
-			# move to that cell
-			agent[4], agent[5] = muscle.move(move_cell[0], move_cell[1])
+				# move to that cell
+				agent[4], agent[5] = muscle.move(move_cell[0], move_cell[1])
 
-			# clamp pos
-			agent[4] = max(1, min(agent[4], (self.display_width//self.grid_size)-2))
-			agent[5] = max(1, min(agent[5], (self.display_height//self.grid_size)-2))
+				# clamp pos
+				agent[4] = max(1, min(agent[4], (self.display_width//self.grid_size)-2))
+				agent[5] = max(1, min(agent[5], (self.display_height//self.grid_size)-2))
 
 		for agent in self.agents:
 			# draw agent
@@ -165,19 +179,27 @@ class Window:
 		agent[7].process_experience(X, sentiment)
 		agent[10].append(sentiment)
 
-	def decide_sentiment(self, agent, X_1, X_2):
-		if X_1 == 0:
-			if X_2 == 0:
+	def decide_sentiment(self, agent, X_1, X_2, X_3):
+		sent = 0
+
+		if agent[2] == 0:
+			if X_1 == 1:
 				sent = -2
 			else:
-				sent = -1
-		elif X_1 == 1:
-			if X_2 == 1:
 				sent = 2
+		elif agent[2] == 1:
+			if X_1 == 2:
+				sent = -2
 			else:
-				sent= 1
-		elif X_1 == 2:
-				sent = 0
+				sent = 2
+		elif agent[2] == 2:
+			if X_1 == 0:
+				sent = -2
+			else:
+				sent = 2
+
+		if agent[3] == X_2:
+			sent = 1
 
 		return sent
 
@@ -264,7 +286,7 @@ class Window:
 		# brain maps
 		section_1_y = 48
 		columns = 15
-		rows = (len(self.agents) // columns)+1
+		rows = ((len(self.agents)-1) // columns)+1
 		size = 36
 		num = 0
 		for yy in range(rows):
@@ -309,7 +331,7 @@ class Window:
 					num += 1
 
 		# agent focus
-		section_2_y = section_1_y + (rows*(size+16)) + 32
+		section_2_y = section_1_y + (rows*(size+16)) + 16
 		if self.focus != None:
 			a = self.agents[self.focus]
 
@@ -333,6 +355,9 @@ class Window:
 
 			for i, exp in enumerate(a[10][-exp_shown_length:]):
 				pygame.draw.rect(self.display, self.sentiment_colors[exp], ((x+220)+i*exp_size, y+section_2_y+20, exp_size, 30))
+
+			text = self.FNT_MEDIUM.render("Last Experience:", True, self.DK_GRAY)
+			self.display.blit(text, (x+220,y+section_2_y))
 
 			agent_experiences = self.FNT_MEDIUM.render("Total Experiences: {}".format(a[7].total_experiences()), True, self.BLACK)
 			self.display.blit(agent_experiences, (x+220,y+section_2_y+20+32))
@@ -361,23 +386,31 @@ class Window:
 			for agent in self.agents:
 				agent_pos_list.append((agent[4], agent[5]))
 
-			width, height = (160//10)-1, (160//10)-1
+			# td minimap
+			width, height = (160//20)-1, (160//20)-1
 			for yy in range(height):
 				for xx in range(width):
-					posxx = a[4]-7+xx
-					posyy = a[5]-7+yy
+					posxx = a[4]-3+xx
+					posyy = a[5]-3+yy
 
-					if xx == 7 and yy == 7:
+					if xx == 3 and yy == 3:
 						col = self.BLACK
+						txt = a[0]
 					else:
 						col = self.GRAY
+						txt = ""
 
 						for a_num, a_pos in enumerate(agent_pos_list):
 							if a_pos[0] == posxx and a_pos[1] == posyy:
 								col = self.colors[self.agents[a_num][2]]
-								break
+								txt = self.agents[a_num][0]
 
-					pygame.draw.rect(self.display, col, (x1+(xx*10)+6, y1+(yy*10)+6, 9, 9))
+					pygame.draw.rect(self.display, col, (x1+(xx*20)+11, y1+(yy*20)+11, 19, 19))
+
+					if txt != "":
+						text = self.FNT_TINY.render("{}".format(txt), True, self.WHITE)
+						text_rect = text.get_rect(center=(x1+(xx*20)+11+10,y1+(yy*20)+11+9))
+						self.display.blit(text, text_rect)
 
 			text = self.FNT_LARGE.render("Eye", True, self.BLACK)
 			self.display.blit(text, (x1, y1-24))
@@ -393,8 +426,35 @@ class Window:
 			self.display.blit(text, (x1, y1-24))
 			pygame.draw.rect(self.display, self.BLACK, (x1, y1, 160, 160), 2)
 
+			# td minimap
+			width, height = 5, 5
+			for yy in range(height):
+				for xx in range(width):
+					posxx = a[4]-2+xx
+					posyy = a[5]-2+yy
+
+					if xx == 2 and yy == 2:
+						col = self.BLACK
+					else:
+						col = self.GRAY
+
+						for a_num, a_pos in enumerate(agent_pos_list):
+							if a_pos[0] == posxx and a_pos[1] == posyy:
+								try:
+									sent = self.agents[a_num][7].predict([[self.agents[a_num][2], self.agents[a_num][3], -1]])
+									col = self.sentiment_colors_alt[sent]
+								except:
+									col = self.DK_GRAY
+
+					pygame.draw.rect(self.display, col, (x1+(xx*20)+32, y1+(yy*20)+32, 19, 19))
+
+			# sent summary
+
+
 			# brain to muscle link
 			pygame.draw.line(self.display, self.BLACK, (x1+160, y1+80), (x1+220, y1+80), 2)
+
+
 
 			# muscle
 			x1, y1 = x+440, section_3_y
